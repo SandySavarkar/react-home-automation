b7ae45a64e1fdea688ec9c2c1cd56ecc8a808b45
Ronak Kapadi <ronakk@itpathsolutions.com>
2022-12-30 10:14:30 +0530

    Basic setup done

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './App.css';
+import Routing from './router/Routing';
+
+const App = ( {...props} ) => {
+  return <Routing {...props} />
+}
+
+export default App;
---
+++ src/utils/localstorage.js
@@ -0,0 +1,4 @@
+export const getDataFromLocal = key => localStorage.getItem(key)
+export const removeDataFromLocal = key => localStorage.removeItem(key)
+export const setDataFromLocal = (key, data) => localStorage.setItem(ket, data)
+export const clearStorage = () => localStorage.clear() 
